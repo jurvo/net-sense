e50f9cbb68e8ba61d1e3370382a013accfcba5e9
jurvo <jurvoel@gmx.de>
2023-08-19 19:31:51 +0200

    added suport for DHT11 sensor

--- Arduino Code/NetSenseNode/NetSenseNode.ino
+++ Arduino Code/NetSenseNode/NetSenseNode.ino
@@ -1,7 +1,16 @@
 #include <ESP8266WiFi.h>
+#include "DHT.h"
 
+// general settings
 int sensorID = 0;
 String hostname = "node-" + String(sensorID);
+#define USESERIAL true;
+
+// DHT11 settings
+#define DHTPIN D4 // data pin
+#define DHTTYPE DHT11 // sensor type
+
+DHT dht(DHTPIN, DHTTYPE); // init the object
 
 // WiFi Settings
 #ifndef STASSID
@@ -17,12 +26,16 @@ int    HTTP_PORT      = 80;
 String HTTP_METHOD    = "GET";
 String HOST_NAME      = "<ip>";
 String TEMP_PATH_NAME = "/net-sense/temp.php";
+String HUMID_PATH_NAME = "/net-sense/humid.php";
 
 void SetUpWiFi()
 {
   // We start by connecting to a WiFi network
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  if (USESERIAL)
+  {
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
+  }
   /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
      would try to act as both a client and an access-point and could cause
      network-issues with your other WiFi-devices on your WiFi-network. */
@@ -32,14 +45,15 @@ void SetUpWiFi()
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+	if (USESERIAL) Serial.print(".");
   }
-  
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  if (USESERIAL)
+  {
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+  }
   // WiFi.mode(WIFI_OFF); // deactivate WiFi but keep credentials
   // WiFi.mode(WIFI_STA); // activate WiFi
   // WiFi.begin();
@@ -53,8 +67,11 @@ void sendDataToWebserver(String path, double value)
   if(client.connect(HOST_NAME, HTTP_PORT)) 
   {
     // if connected:
-    Serial.print("Connected to server: ");
-    Serial.println(HOST_NAME + path + queryString);
+	if (USESERIAL)
+	{
+		Serial.print("Connected to server: ");
+		Serial.println(HOST_NAME + path + queryString);
+	}
     // make a HTTP request:
     // send HTTP header
     client.println(HTTP_METHOD + " " + path + queryString + " HTTP/1.1");
@@ -66,25 +83,34 @@ void sendDataToWebserver(String path, double value)
       if(client.available()){
         // read an incoming byte from the server and print it to serial monitor:
         char c = client.read();
-        Serial.print(c);
+		if (USESERIAL) Serial.print(c);
       }
     }
     // the server's disconnected, stop the client:
     client.stop();
-    Serial.println("Disconnected.");
+    if (USESERIAL) Serial.println("Disconnected.");
   }
   else // if not connected:
   {
-    Serial.println("connection failed");
+    if (USESERIAL) Serial.println("connection failed");
   }
 }
 
-
 void setup() {
-  Serial.begin(9600);
-  SetUpWiFi();
-  sendDataToWebserver(TEMP_PATH_NAME, random(15,25));
-  ESP.deepSleep(60e6);
+	dht.begin();
+	if (USESERIAL) Serial.begin(9600);
+	float h = dht.readHumidity();
+  	float t = dht.readTemperature();
+	if ((isnan(h) || isnan(t)) && USESERIAL) {
+    	Serial.println(F("Failed to read from DHT sensor!"));
+	}
+	else
+	{
+		SetUpWiFi();
+		sendDataToWebserver(TEMP_PATH_NAME, t);
+		sendDataToWebserver(HUMID_PATH_NAME, h);
+	}
+	ESP.deepSleep(60e6);
 }
 
 void loop() {
